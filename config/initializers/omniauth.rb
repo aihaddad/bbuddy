e07c3c71a7c83eb7b0d0617281003a24c34ee9e2
Ahmed Elhaddad <aihaddad@outlook.com>
2015-06-28 16:02:44 +0300

    Adds middlewares needed for OmniAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
+Rails.application.config.middleware.use ActionDispatch::Cookies
+Rails.application.config.middleware.use Rails.application.config.session_store,
+                                        Rails.application.config.session_options
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,
             Rails.application.secrets.facebook_key,
             Rails.application.secrets.facebook_secret
 end
+# needed for Doorkeeper /oauth views
+Rails.application.config.middleware.use ActionDispatch::Flash
